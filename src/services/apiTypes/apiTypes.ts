0dec59e58af769e0061bc0e15865997408d8725b
wrunika <2805nika@gmail.com>
2024-04-11 00:04:38 +0300

    feat: add tasksApi with types

--- src/services/apiTypes/apiTypes.ts
+++ src/services/apiTypes/apiTypes.ts
@@ -2,3 +2,24 @@ export type LoginRequestType = {
   login: string
   password: string
 }
+
+export type GetTaskRequestType = {
+  answers: []
+  ars: null
+  description: string
+  file: null
+  id: number
+  image: string
+  imageStr: string
+  level: {
+    id: number
+    name: 'HIGH' | 'LOW' | 'MIDDLE'
+  }
+  name: string
+  nextTaskId: number
+  strAnswers: null
+  type: {
+    id: number
+    name: 'CHECKBOX' | 'RADIO'
+  }
+}
