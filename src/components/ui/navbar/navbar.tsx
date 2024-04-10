eca78bee1a6c011c7048e23bcbeb42c91ff0f3b0
wrunika <2805nika@gmail.com>
2024-04-11 00:08:25 +0300

    feat: add routes and navbar

---
+++ src/components/ui/navbar/navbar.tsx
@@ -0,0 +1,67 @@
+import { useTranslation } from 'react-i18next'
+import { NavLink } from 'react-router-dom'
+
+import { ROUTES } from '@/constants'
+import { BsPersonPlus } from 'react-icons/bs'
+import { IoPeopleOutline } from 'react-icons/io5'
+import { IoSettingsOutline } from 'react-icons/io5'
+import { MdOutlineTaskAlt } from 'react-icons/md'
+import { MdOutlineAddTask } from 'react-icons/md'
+
+import style from './navbar.module.scss'
+
+export const Navbar = () => {
+  const { t } = useTranslation()
+
+  return (
+    <nav>
+      <ul className={style.navbarWrapper}>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? style.active : style.nav)}
+            to={ROUTES.main}
+          >
+            <MdOutlineTaskAlt />
+            {t('navbar.allTasks')}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? style.active : style.nav)}
+            to={ROUTES.createTask}
+          >
+            <MdOutlineAddTask />
+            {t('navbar.create')}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? style.active : style.nav)}
+            to={ROUTES.allAccesses}
+          >
+            <IoPeopleOutline />
+            {t('navbar.allAccesses')}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? style.active : style.nav)}
+            to={ROUTES.createAccess}
+          >
+            <BsPersonPlus />
+            {t('navbar.createAccess')}
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? style.active : style.nav)}
+            to={ROUTES.settings}
+          >
+            <IoSettingsOutline />
+            {t('navbar.settings')}
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,9 @@
 export const ROUTES = {
+  allAccesses: '/admin/accesses/all',
+  createAccess: '/admin/accesses/create',
+  createTask: '/admin/tasks/create',
   error: '*',
   login: '/login',
-  main: '/',
+  main: '/admin/tasks/all',
+  settings: '/admin/settings',
 } as const
